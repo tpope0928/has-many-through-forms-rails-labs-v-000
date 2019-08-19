1e296e3c69761aa155028c95823d7dcce177724c
Thomas Pope <tpope0928@gmail.com>
2019-08-19 01:18:47 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,13 @@ class CommentsController < ApplicationController
 
   def create
     comment = Comment.create(comment_params)
+    # binding.pry
     redirect_to comment.post
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username, :email])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
   end
 end
