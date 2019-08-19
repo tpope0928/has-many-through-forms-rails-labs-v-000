b36d2806d4608eeaac46ff41db35088473f86de3
Thomas Pope <tpope0928@gmail.com>
2019-08-19 01:29:23 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+
 class User < ActiveRecord::Base
-  has_many :comments
-  has_many :posts, through: :comments
+has_many :comments
+has_many :posts, through: :comments
+validates :username, uniqueness: true
+
 end
