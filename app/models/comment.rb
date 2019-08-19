f280f5bfe6acf7414ae06dac4bc7b82ae4430969
Thomas Pope <tpope0928@gmail.com>
2019-08-19 01:09:27 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepted_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
 
 end
