c572a59c1e84dcd24a5eb2adb71ce27b0438adf6
Thomas Pope <tpope0928@gmail.com>
2019-08-19 01:20:08 +0000

    Automatically backed up by Learn

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
+
 class Category < ActiveRecord::Base
-  has_many :post_categories
-  has_many :posts, through: :post_categories
+has_many :post_categories
+has_many :posts, through: :post_categories
+
+
 end
